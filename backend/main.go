cd824c792dc3d23a65eefa49307fdc4679f279cd
Gautham <gauthamskaniyattil@gmail.com>
2024-07-26 01:34:11 +0530

    Created Server
    Created The Server and Tested Read ALL Route  : Sucess

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	router "github.com/its-dev24/off-campus-placement-tracker/Router"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error while Loading Environment Variable : main.go : ", err)
+	}
+	port := os.Getenv("PORT")
+	mux := router.Router()
+
+	server := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error While Running Server : main.go : ", err)
+	}
 }
