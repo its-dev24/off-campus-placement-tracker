e4e1fbbee5abe4a078ba848f6fcd25352eab905e
Gautham <gauthamskaniyattil@gmail.com>
2024-07-26 21:39:23 +0530

    Added MongoDB connection

--- backend/main.go
+++ backend/main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"fmt"
 
-	router "github.com/its-dev24/off-campus-placement-tracker/Router"
-	"github.com/joho/godotenv"
+	db "github.com/its-dev24/off-campus-placement-tracker/DB"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error while Loading Environment Variable : main.go : ", err)
-	}
-	port := os.Getenv("PORT")
-	mux := router.Router()
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error while Loading Environment Variable : main.go : ", err)
+	// }
+	// port := os.Getenv("PORT")
+	// mux := router.Router()
 
-	server := http.Server{
-		Addr:    port,
-		Handler: mux,
-	}
+	// server := http.Server{
+	// 	Addr:    port,
+	// 	Handler: mux,
+	// }
 
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Error While Running Server : main.go : ", err)
-	}
+	// err = server.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatal("Error While Running Server : main.go : ", err)
+	// }
+	fmt.Println(db.MongoCol)
 }
