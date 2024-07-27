d09e641831ad044fecea8ed61a22dc1cc361064b
Gautham <gauthamskaniyattil@gmail.com>
2024-07-27 20:41:44 +0530

    Added Delete Single Application Helper

--- backend/DB/dbhelper.go
+++ backend/DB/dbhelper.go
@@ -7,6 +7,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+//Function To Find All Applications
+
 func ReadAllApplications() ([]modal.Job, error) {
 	cur, err := MongoCol.Find(context.Background(), bson.M{})
 	if err != nil {
@@ -16,10 +18,25 @@ func ReadAllApplications() ([]modal.Job, error) {
 	cur.All(context.Background(), &jobs)
 	return jobs, nil
 }
+
+//Function To Delete All Applications
+
 func DeleteAllApplications() (int, error) {
 	deleteResult, err := MongoCol.DeleteMany(context.Background(), bson.M{})
 	if err != nil {
 		return 0, err
+	}
+	return int(deleteResult.DeletedCount), nil
+}
+
+//Function To Delete A Single Job
+
+func DeleteASingleApplication(id string) (int, error) {
+
+	filter := bson.M{"_id": id}
+	deleteResult, err := MongoCol.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return 0, nil
 	}
 	return int(deleteResult.DeletedCount), nil
 }
